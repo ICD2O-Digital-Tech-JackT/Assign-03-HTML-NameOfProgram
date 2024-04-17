b37832101ad7fd63be9c81b81afb440fff977222
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-04-17 14:42:45 +0000

    Finished but fr this time

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-function Display(Area, Perimeter) {
-  if (Area) {
-    document.getElementById("Output").innerHTML = "The area of this hexagon is " + Area.toFixed(2) + "cm<sup>2</sup><br>The permieter of this hexagon is " + Perimeter.toFixed(2) + "cm";
-  } else {
-    document.getElementById("Output").innerHTML = "Please enter a valid number"
-  }
-}
+/* function to calculate the area and perimeter */
 function Calculate() {
   let SideA = document.getElementById("sideA").value
   let Area = 3 * Math.sqrt(3) / 2 * Math.pow(SideA, 2)
   let Perimeter = SideA * 6
-  Display(Area, Perimeter)
+
+  return [Area, Perimeter];
+}
+
+/* function to display the area and perimeter */
+function Display() {
+  let Array = Calculate();
+  document.getElementById("Output").innerHTML = Array[0]
+  if (Array[0]) {
+    document.getElementById("Output").innerHTML = "The area of this hexagon is " + Array[0].toFixed(2) + "cm<sup>2</sup><br>The permieter of this hexagon is " + Array[1].toFixed(2) + "cm";
+  } else {
+    document.getElementById("Output").innerHTML = "Please enter a valid number"
+  }
 }
